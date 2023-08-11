fb7c5826cc007704205e1ee70f33226dd3d9ea0d
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-08-12 01:49:58 +0600

    Stage-1 Task I/O

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,75 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.Arrays;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        String fileContent = readFileIntoString(file);
+        System.out.println(fileContent);
+        String[] keyValues = parseFileContentForKeyValue(fileContent);
+        System.out.println(Arrays.toString(keyValues));
+        return mapDataToProfileObject(keyValues);
+    }
+
+    // Create Profile
+    private Profile mapDataToProfileObject(String[] keyValues) {
+        Profile p = new Profile();
+        String[] pair;
+        for (int i = 0; i < keyValues.length; i++) {
+            pair = keyValues[i].split("\\s");
+            switch (pair[0]) {
+                case "Name:":
+                    p.setName(pair[1]);
+                    break;
+                case "Age:":
+                    p.setAge(Integer.valueOf(pair[1]));
+                    break;
+                case "Email:":
+                    p.setEmail(pair[1]);
+                    break;
+                case "Phone:":
+                    p.setPhone(Long.valueOf(pair[1]));
+                    break;
+            }
+        }
+        return p;
+    }
+
+    // Parse this string for key-value pairs
+    private String[] parseFileContentForKeyValue(String fileContent) {
+        String[] pairs = fileContent.split("\\n");
+        return pairs;
+    }
+
+    // Reading file data into string
+    private String readFileIntoString(File file) {
+        String content = "";
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new java.io.FileReader(file.getPath()));
+            String buffer = "";
+            while ((buffer = br.readLine()) != null) {
+                // buffer = br.readLine();
+                content = content + buffer.strip() + "\n";
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return content.strip();
     }
 }
---
+++ src/main/java/com/epam/mjc/io/Main.java
@@ -0,0 +1,16 @@
+package com.epam.mjc.io;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File f = new File("I:\\mjc-school\\stage1-module6-io-task1\\src\\main\\resources\\Profile.txt");
+
+        FileReader fr = new FileReader();
+
+        Profile profile = fr.getDataFromFile(f);
+
+        System.out.println(profile);
+    }
+}
