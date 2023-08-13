30ca14a90042ec8fa479d698b42926edca807ab4
Sergey Tsoy <sergeytsoy740@gmail.com>
2023-08-13 23:44:09 +0600

    Stage-1 Task I/O (5)

---
+++ src/main/java/com/epam/mjc/io/Main.java
@@ -0,0 +1,15 @@
+package com.epam.mjc.io;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String s = File.separator;
+
+        String relativePath =
+                "src" + s + "main" + s + "resources" + s + "Profile.txt"; // valid
+
+        Profile profile = new FileReader().getDataFromFile(new File(relativePath));
+    }
+}
